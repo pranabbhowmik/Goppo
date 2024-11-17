a986328552549a86c7c41ef452e1693e280615f7
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-17 14:30:56 +0530

    Update favicon link and adjust logo size on login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <img src={logo} alt="logo" className="w-20 h-20 mx-auto" />
+        <img src={logo} alt="logo" className="w-28 h-28 mx-auto" />
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           Login
           <span className="text-blue-500"> Goppo</span>
